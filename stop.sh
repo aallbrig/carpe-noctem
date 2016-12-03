c7aa06fc0c5af4172bf5c18c1a8deb241038b1be
Andrew Allbright <aallbrig@gmail.com>
2016-12-03 14:32:47 -0500

    Update stop.sh with updated reference to new dir "provisioning"

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 # vagrant global-status | grep virtualbox | cut -c 1-9 | while read line; do echo $line; vagrant halt $line; done;
 
-ansible-playbook plays/teardown-dev-machine.yml -i inventories/dev/hosts.ini
+ansible-playbook provisioning/plays/teardown-dev-machine.yml -i inventories/dev/hosts.ini
 
 vagrant destroy --force
 
