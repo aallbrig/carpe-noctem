d1dff32744d53af8e45ea15c4be51dd018b07167
Andrew Allbright <aallbrig@gmail.com>
2016-12-05 20:05:48 -0500

    Basic spritesheet example and movement.

--- source/web/static/src/index.js
+++ source/web/static/src/index.js
@@ -3,33 +3,19 @@ const app = (() => {
   const canvasW = 800;
   const canvasH = 600;
   let sprite;
-  const game = new Phaser.Game(
-    canvasW,
-    canvasH,
-    Phaser.AUTO,
+  const game = new Phaser.Game(1080, 1080, Phaser.AUTO,
     canvasId,
     {
       preload: () => {
         game.load.spritesheet('mech', 'assets/MechSheet-Raw.png', 48, 48, 12);
-        console.log('on preload');
-        document.write('O n P h a s e r P r e l o a d C o m p l e t e . . .');
       },
       create: () => {
-        sprite = game.add.sprite(40, 100, 'mech');
+        sprite = game.add.sprite(48, 48, 'mech');
         sprite.animations.add('walk');
-        sprite.animations.play('walk', 50, true);
+        sprite.animations.play('walk', 48, true);
         game.add.tween(sprite).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);
-
-        console.log('on create');
-        document.write("O n P h a s e r C r e a t e C o m p l e t e . . .");
-        document.write("O n P h a s e r U p d a t e I n i t i a l i z i n g... Done.  Check Console.");
       },
       update:  () => {
-        if (sprite.x >= 300) {
-          sprite.scale.x += 0.01;
-          sprite.scale.y += 0.01;
-        }
-        console.log('On Phaser update');
       }
     }
   );
