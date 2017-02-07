d0e50e864c30ff80096cfba25570685efb53caac
Andrew Allbright <aallbrig@gmail.com>
2017-02-06 19:08:14 -0500

    WIP Silly looking scenario is playing out.  Run application and move arround using arrow keys or WASD.

--- source/web/static/src/components/presentation/PhaserRunner.js
+++ source/web/static/src/components/presentation/PhaserRunner.js
@@ -11,6 +11,9 @@ class PhaserRunner extends React.Component {
     let sprite;
     let player;
     let cursors;
+    let moveSprite;
+    let playerCollisionGroup;
+    let spriteCollisionGroup;
     const game = new Phaser.Game(
       width,
       height,
@@ -18,6 +21,10 @@ class PhaserRunner extends React.Component {
       findDOMNode(canvas),
       {
         preload: () => {
+          // game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+          // game.scale.pageAlignHorizontally = true;
+          // game.scale.pageAlignVertically = true;
+          game.stage.backgroundColor = '#eee';
           game.load.spritesheet('mech', 'assets/MechSheet-Raw.png', 48, 48, 12);
           game.load.image('sandrock', 'assets/sandrock.png');
           game.load.image('deathscythe', 'assets/deathscythe.png');
@@ -25,38 +32,66 @@ class PhaserRunner extends React.Component {
         create: () => {
           game.world.setBounds(0, 0, 1920, 1920);
           game.physics.startSystem(Phaser.Physics.P2JS);
+          game.physics.p2.setImpactEvents(true);
+          playerCollisionGroup = game.physics.p2.createCollisionGroup();
+          spriteCollisionGroup = game.physics.p2.createCollisionGroup();
+          game.physics.p2.updateBoundsCollisionGroup();
 
           sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'deathscythe');
-          sprite.height = 100;
-          sprite.width = 100;
-
-          game.add.tween(sprite)
-            .to({ x: game.width }, Math.random() * 5000, Phaser.Easing.Linear.None, true);
-          player = game.add.sprite(game.world.centerX, game.world.centerY, 'sandrock');
-          player.height = 100;
-          player.width = 100;
-
+          game.physics.p2.enable(sprite);
+          sprite.height = 200;
+          sprite.width = 200;
+          sprite.body.setCollisionGroup(spriteCollisionGroup);
+          // moveSprite = () => game.add.tween(sprite)
+          //   .to({
+          //     x: game.world.centerX/*game.world.bounds.width * Math.random()*/,
+          //     y: game.world.centerY/*game.world.bounds.height * Math.random()*/
+          //   }, 10000, Phaser.Easing.Linear.None, true);
+          // moveSprite();
+          player = game.add.sprite(game.world.centerX + 200, game.world.centerY, 'sandrock');
+          player.height = 400;
+          player.width = 400;
+          sprite.scale.set(0.2);
           game.physics.p2.enable(player);
+          player.body.setCollisionGroup(playerCollisionGroup);
+          player.body.collides([playerCollisionGroup, spriteCollisionGroup]);
+          console.log(playerCollisionGroup);
+          // playerCollisionGroup.collides(spriteCollisionGroup, () => {
+          //   console.log('collision!');
+          //   // moveSprite();
+          //   game.camera.shake(0.05, 500);
+          // }, this);
           game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
           cursors = game.input.keyboard.createCursorKeys();
         },
         update: () => {
+          const {W, A, S, D} = Phaser.Keyboard;
           game.debug.cameraInfo(game.camera, 32, 32);
           game.debug.spriteCoords(player, 32, 500);
-          player.body.setZeroVelocity();
-          const {W, A, S, D} = Phaser.Keyboard;
 
+          player.body.setZeroVelocity();
           if (cursors.up.isDown || game.input.keyboard.isDown(W)) {
-              player.body.moveUp(300);
+            player.body.moveUp(300);
           } else if (cursors.down.isDown || game.input.keyboard.isDown(S)) {
-              player.body.moveDown(300);
+            player.body.moveDown(300);
           }
           if (cursors.left.isDown || game.input.keyboard.isDown(A)) {
-              player.body.velocity.x = -300;
+            player.body.moveLeft(300);
           } else if (cursors.right.isDown || game.input.keyboard.isDown(D)) {
-              player.body.moveRight(300);
+            player.body.moveRight(300);
           }
-          
+
+          // If player is colliding with sprite
+          game.physics.arcade.collide(playerCollisionGroup, spriteCollisionGroup, () => {
+            console.log('Is colliding');
+            game.camera.shake(0.05, 500);
+            moveSprite();
+          }, null, this);
+          game.physics.arcade.collide(sprite, player, () => {
+            console.log('asklfjasj colliding');
+            game.camera.shake(0.05, 500);
+            moveSprite();
+          });
         }
       }
     );
