187fb52efc094632fd8a57ac92cab7b6a17b9d4f
Andrew Allbright <aallbrig@gmail.com>
2017-03-19 13:06:39 -0400

    Utilize history provided by react-router instead of using my own.

--- source/web/static/src/index.tsx
+++ source/web/static/src/index.tsx
@@ -3,14 +3,13 @@ import 'bootswatch/united/bootstrap.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
-import { createHashHistory } from 'history';
 import Routes from './routers/Routes';
 import store from './store/store';
 // TODO: Resolve situation where History !== History.History, even though they both reference the same type..?
 const history = syncHistoryWithStore(
-  createHashHistory(),
+  hashHistory,
   store
 );
 ReactDOM.render(
