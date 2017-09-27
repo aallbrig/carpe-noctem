c0993264fb2f04b00a4c0779898dd09f6234a784
Andrew's 13" 512GB i5 <aallbrig@gmail.com>
2017-09-27 19:31:14 -0400

    Added game over text.

--- source/web/static/src/classes/MoonShooter/states/GameOver.ts
+++ source/web/static/src/classes/MoonShooter/states/GameOver.ts
@@ -1,6 +1,19 @@
 import { State } from 'phaser';
 
 export class GameOver extends State {
+    public create() {
+        const style = {
+          font: '65px Arial', fill: '#ff0000', align: 'center'
+        };
+        this.game.add.text(20, 20, 'Game Over', style);
+        this.game.add.text(
+            20,
+            100,
+            `Click or tap
+to restart`,
+            style
+        );
+    }
     public update() {
         if (
             this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)
